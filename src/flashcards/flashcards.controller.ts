2220e274ea14c682a3ffdc1ab72497787565e900
Edward Tanguay (wk) <edward@tanguay.info>
2020-11-30 17:37:59 +0100

    add getid

--- src/flashcards/flashcards.controller.ts
+++ src/flashcards/flashcards.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, Redirect, Param } from '@nestjs/common';
 
 @Controller('flashcards')
 export class FlashcardsController {
-  @Get()
-  findAll(@Req() request: Request): string {
-    return 'This shows all flashcards';
-  }
+	@Post()
+	create(): string {
+		return 'This action adds a new flashcard';
+	}
+
+	@Get()
+	findAll(): string {
+		return 'This action returns all flashcards';
+	}
+
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return `This action returns a #${id} flashcard`;
+	}
 }
+
+
+
+
